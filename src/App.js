f62f5fefe1f896d52e9d59740ce3914f69eb57de
DBohlke <78039823+DBohlke@users.noreply.github.com>
2021-07-25 23:01:56 -0500

    Added photo API and component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,24 @@ export default function App() {
           <Dictionary defaultKeyword="lion" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Dani Bohlke</small>
+          <small>
+            This project was coded by Dani Bohlke and is{" "}
+            <a
+              href="https://github.com/DBohlke/react-weather-app-new"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on Github
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://friendly-feynman-0e5d15.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
